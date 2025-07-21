ea85daee800175b30c7574084b193bf3f073d984
Abuzar Sheikh <abuzarssheikh3@gmail.com>
2025-07-21 12:10:04 +0530

    hm

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const API_BASE_URL = import.meta.env.MODE === 'development'
-  ? '/api'
-  : 'https://money-talks-bfcb.vercel.app/api';
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://money-talks-bfcb.vercel.app',
+      '/api': 'http://localhost:4000',
     },
   }
 })
